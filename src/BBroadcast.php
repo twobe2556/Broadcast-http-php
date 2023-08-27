6e81147a1f2e198035dec44065ed4005ae034fb9
PC Bank <nutthapong.phensuk@gmail.com>
2023-08-27 22:21:30 +0700

    firstcommit

---
+++ src/BBroadcast.php
@@ -0,0 +1,36 @@
+<?php
+require 'vendor/autoload.php';
+
+use WebSocket\Client;
+
+class WebSocketPusher
+{
+    private $client;
+
+    public function __construct($websocketUrl)
+    {
+        $this->client = new Client($websocketUrl);
+    }
+
+    public function trigger($channel, $event, $data)
+    {
+        try {
+            $message = [
+                'channel' => $channel,
+                'event' => $event,
+                'message' => $data
+            ];
+
+            @$this->client->send(json_encode($message));
+            $this->Close();
+        } catch (\Throwable $th) {
+            // throw $th;
+            echo "Error: Disconnected from the server";
+        }
+    }
+
+    public function close()
+    {
+        @$this->client->close();
+    }
+}
